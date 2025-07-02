387434b53f53ac1a6646495b04a84e660d076a15
serii burduja <seriiburduja@gmail.com>
2025-07-02 16:40:48 +0300

    feat: input validator

--- modules/findInFiles.py
+++ modules/findInFiles.py
@@ -1,12 +1,8 @@
-import os
-from rich import print
 from rich.console import Console
-from rich.panel import Panel
 from pyfzf.pyfzf import FzfPrompt
 
-from utils.getAttachedDirs import getAttachedDirs
+from classes.InputValidator import InputValidator
 from utils.getExcludedDirs import getExcludedDirs
-from utils.getFileData import getFileData
 from utils.showOccuriences import showOccuriences
 fzf = FzfPrompt()
 console = Console()
@@ -15,28 +11,11 @@ console = Console()
 def findInFiles():
     """find all files with the specified extension
         and exclude directories with bash and grep"""
-    file_extension, search_string, _ = getFileData(replace=False)
+    string_to_search = InputValidator.get_string(
+        "Enter the string to search for: ")
+
+    file_extension = InputValidator.get_string(
+        "Enter the file extension to search in (e.g., 'py', 'txt'): ")
+
     excluded_dirs = getExcludedDirs()
-    showOccuriences(file_extension, search_string, excluded_dirs)
-    excluded_dirs = getExcludedDirs()
-    attached_dirs = getAttachedDirs()
-    print(Panel(f"[red]excluded_dirs: {excluded_dirs}"))
-    print(Panel(f"[blue]attached_dirs: {attached_dirs}"))
-
-    # Base command
-    base_cmd = (
-        f"ack --type-add={file_extension}:*.{file_extension} "
-        f"--type={file_extension} -r '{search_string}'"
-    )
-
-    # Add attached_dirs if any
-    if attached_dirs:
-        base_cmd += ' ' + ' '.join(attached_dirs)
-
-    # Add exclusions if any
-    if excluded_dirs:
-        for dir in excluded_dirs:
-            base_cmd += f" --ignore-dir={dir}"
-
-    print(Panel(f"[green]command: {base_cmd}"))
-    os.system(base_cmd)
+    showOccuriences(file_extension, string_to_search, excluded_dirs)
