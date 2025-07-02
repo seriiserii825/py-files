081d66775487f9aec1d4bd0e85a86dab1d4cbae7
serii burduja <seriiburduja@gmail.com>
2025-07-02 16:26:50 +0300

    feat: getListDir

--- modules/getListDir.py
+++ modules/getListDir.py
@@ -1,11 +1,10 @@
 import os
-from rich import print
+
+
 def getListDir(base_path):
     """
     List all directories in a given path.
     """
-    from rich.console import Console
-    from pyfzf.pyfzf import FzfPrompt
-
     # Get the list of directories
-    return [d for d in os.listdir(base_path) if os.path.isdir(os.path.join(base_path, d))]
+    return [d for d in os.listdir(base_path)
+            if os.path.isdir(os.path.join(base_path, d))]
